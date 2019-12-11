21435be6f75dbda37fe18527978a6dacd1f5e229
ierven <ierven@umich.edu>
2019-12-11 16:30:41 -0500

    new json files

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import json
 import os
 
 filename = "reviews.json"
+filename_count = "count.json"
 full_path = os.path.join(os.path.dirname(__file__), filename)
+full_path_count = os.path.join(os.path.dirname(__file__), filename_count)
+
+with open(full_path_count, 'r') as count_file:
+    fileData = json.load(count_file)
+    city_idx = fileData['count']
+
+
+cities = ["Ann Arbor, MI", "Athens, OH", "Bloomington, IN", "Madison, WI", "Syracuse, NY", "Berkeley, CA", "Boulder, CO", "Tempe, AZ", "Malibu, CA", "Corvallis, OR"]
+
+city = cities[city_idx]
 
 API_KEY = "aGUAFSIk5qetLlnhr5z6G93ew_cx3DYT2nT2nqeRs34n-mZVZ5j-msEqj_LvGOyU3SuXv6d45ZVbodjTnPzXuIYt-mleQEaVjATrWAKT2zPPI0hk_juoc3kGAHnoXXYx"
-search_url = "https://api.yelp.com/v3/businesses/search?location=\"Ann Arbor, MI\"&radius=30000"
+search_url = "https://api.yelp.com/v3/businesses/search?location=\"{}\"&radius=30000".format(city)
 review_url = "https://api.yelp.com/v3/businesses/{}/reviews"
 
 headers = {
@@ -29,7 +40,7 @@ for bus in bus_dict.keys():
     price1 = bus_dict[bus][2]
     res_data = requests.request('GET', review_url.format(id1), headers=headers)
     parsed_data = res_data.json()
-    review_data[bus] = (parsed_data['reviews'], rating1, price1)
+    review_data[bus] = (parsed_data['reviews'], rating1, price1, city)
 
 for rest in review_data.keys():
     listOfReviews = []
@@ -41,5 +52,6 @@ for rest in review_data.keys():
 
 with open(full_path, 'w') as outfile:
     json.dump(review_data, outfile)
+
 
 print(review_data)
--- testing_api.py
+++ testing_api.py
@@ -9,9 +9,9 @@ full_path = os.path.join(os.path.dirname(__file__), filename)
 thesKey = "31d9d27b-4af0-4573-b493-842b62cc47d1"
 url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key=31d9d27b-4af0-4573-b493-842b62cc47d1"
 
-posWords = ["impressed", "great", "excellent", "stellar", "delicious", "fast", "tasty", "flavorful", "nice", "fun", "welcoming", "clean", "efficient", "good", "wonderful"]
+posWords = ["friendly", "fresh", "awesome", "kind", "hype", "super", "unique","gourmet", "ambiance", "glad", "healthy", "comfortable", "favorite", "top","haven", "love", "impressed", "great", "excellent", "stellar", "delicious", "fast", "tasty", "flavorful", "nice", "fun", "welcoming", "clean", "efficient", "good", "wonderful", "best", "enjoyable"]
 posWordsExtended = []
-posWordsDict = {}
+wordsDict = {}
 
 for word in posWords:
     posWordsExtended.append(word)
@@ -23,9 +23,22 @@ for word in posWords:
         if word not in posWordsExtended:
             posWordsExtended.append(word)
 
-posWordsDict["pos_words"] = posWordsExtended
+wordsDict["pos_words"] = posWordsExtended
 
-negWords = ["bad", "horrible", "yucky", "terrible", "rotten", "slow", "disgusting", "rude", "mean", ]
+negWords = ["disappointed", "sad", "bottom", "bad", "horrible", "yucky", "terrible", "rotten", "slow", "disgusting", "rude", "mean", "cold", "boring", "expensive", "awful", "poor", "worst", "closed"]
+negWordsExtended = []
+
+for word in negWords:
+    negWordsExtended.append(word)
+for word in negWords:
+    url_word = url.format(word)
+    badData = requests.get(url_word).json()
+    syns = badData[0]['meta']['syns'][0]
+    for word in syns:
+        if word not in posWordsExtended:
+            negWordsExtended.append(word)
+wordsDict["neg_words"] = negWordsExtended
+
 with open(full_path, 'w') as outfile:
-    json.dump(posWordsDict, outfile)
+    json.dump(wordsDict, outfile)
 print(posWordsExtended)
